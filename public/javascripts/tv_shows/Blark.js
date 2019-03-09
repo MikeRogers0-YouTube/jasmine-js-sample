a42fa0f7e7faa1c0d47269db4f7b7e6df12f882e
Mike Rogers <me@mikerogers.io>
2019-03-09 14:16:55 +0000

    Making test pass

--- public/javascripts/tv_shows/Blark.js
+++ public/javascripts/tv_shows/Blark.js
@@ -3,10 +3,13 @@ function Blark() {
 }
 
 Blark.prototype.sayHi = function() {
+  return 'Hi Son';
 };
 
 Blark.prototype.startParenting = function() {
+  this.isParenting = true;
 };
 
 Blark.prototype.stopParenting = function() {
+  this.isParenting = false;
 };
